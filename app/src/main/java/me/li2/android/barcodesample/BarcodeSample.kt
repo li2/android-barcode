8229b00e4edb3974625f86cab4c785c29e343118
Weiyi Li <weiyi.just2@gmail.com>
2021-12-11 20:47:59 +1300

    Updated sample

--- app/src/main/java/me/li2/android/barcodesample/BarcodeSample.kt
+++ app/src/main/java/me/li2/android/barcodesample/BarcodeSample.kt
@@ -5,12 +5,13 @@ import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.FragmentActivity
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import io.reactivex.rxjava3.disposables.Disposable
+import me.li2.android.barcode.BarcodeScanner
 import me.li2.android.common.framework.openAppSettings
 import me.li2.android.common.rx.PermissionResult.*
 import me.li2.android.common.rx.checkAndRequestCameraPermission
 import me.li2.android.view.popup.snackbar
 
-fun cameraPermissionPrompt(context: Context): AlertDialog {
+private fun cameraPermissionPrompt(context: Context): AlertDialog {
     return MaterialAlertDialogBuilder(context)
         .setTitle("\"Demo App\" Would Like to Access the Camera")
         .setMessage("This will let you read QR codes, take photos and record videos")
@@ -19,13 +20,21 @@ fun cameraPermissionPrompt(context: Context): AlertDialog {
         .create()
 }
 
-fun FragmentActivity.doWithCameraPermission(action: () -> Unit): Disposable {
+private fun FragmentActivity.doWithCameraPermission(onGranted: () -> Unit): Disposable {
     return checkAndRequestCameraPermission(cameraPermissionPrompt(this))
         .subscribe { permissionResult ->
             when (permissionResult) {
-                GRANTED -> action()
+                GRANTED -> onGranted()
                 DENIED -> snackbar("The functionality is not available without camera permission!")
                 DENIED_NOT_ASK_AGAIN -> openAppSettings(applicationContext.packageName)
             }
         }
+}
+
+fun scan(activity: FragmentActivity) {
+    activity.doWithCameraPermission {
+        BarcodeScanner.scan(activity).subscribe { content ->
+            if (content.isNotEmpty()) activity.snackbar(content)
+        }
+    }
 }
--- app/src/main/java/me/li2/android/barcodesample/MainActivity.kt
+++ app/src/main/java/me/li2/android/barcodesample/MainActivity.kt
@@ -1,26 +1,16 @@
 package me.li2.android.barcodesample
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
-import me.li2.android.barcode.BarcodeScanner
-import me.li2.android.view.popup.snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        barcode_scan_button.setOnClickListener {
-            scan()
-        }
-    }
-
-    private fun scan() {
-        doWithCameraPermission {
-            BarcodeScanner.scan(this).subscribe { content ->
-                if (content.isNotEmpty()) snackbar(content)
-            }
+        findViewById<Button>(R.id.barcode_scan_button).setOnClickListener {
+            scan(this)
         }
     }
 }
